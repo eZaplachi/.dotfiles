6edb347d9487566d8ed9e3ab6eea0aaf3134e46d
eZaplachi <evan@zaplachi.ca>
2023-03-22 19:41:04 -0600

    Updated neovim config to use lazyvim

--- install.sh
+++ install.sh
@@ -9,28 +9,28 @@ curl -L https://nixos.org/nix/install | sudo sh
 # Install Packages
 nix-env -iA \
 	nixpkgs.fish \
-        nixpkgs.git \
-        nixpkgs.gcc \
-        nixpkgs.lua \
-        nixpkgs.luajit \
-        nixpkgs.luajitPackages.luarocks-nix \
-        nixpkgs.nodejs \
+	nixpkgs.git \
+	nixpkgs.gcc \
+	nixpkgs.lua \
+	nixpkgs.luajit \
+	nixpkgs.luajitPackages.luarocks-nix \
+	nixpkgs.nodejs \
 	nixpkgs.nodePackages.pnpm \
-        nixpkgs.deno \
-        nixpkgs.php82 \
-        nixpkgs.php82Packages.composer \
-        nixpkgs.python3Full \
+	nixpkgs.deno \
+	nixpkgs.php82 \
+	nixpkgs.php82Packages.composer \
+	nixpkgs.python3Full \
 	nixpkgs.python3Packages.pip \
 	nixpkgs.rustup \
-        nixpkgs.go \
-        nixpkgs.zulu \
+	nixpkgs.go \
+	nixpkgs.zulu \
 	nixpkgs.tmux \
 	nixpkgs.bat \
 	nixpkgs.fzf \
 	nixpkgs.ripgrep \
-        nixpkgs.fd \
-        nixpkgs.gnumake \
-        nixpkgs.xdg-utils \
+	nixpkgs.fd \
+	nixpkgs.gnumake \
+	nixpkgs.xdg-utils \
 	nixpkgs.stow \
 	nixpkgs.gh \
 	nixpkgs.unzip \
@@ -42,7 +42,6 @@ command -v fish | sudo tee -a /etc/shells
 #sudo sed s/required/sufficient/g -i /etc/pam.d/chsh # PAM auth failed workaround
 #sudo chsh -s $(which fish) $USER
 
-
 # Fish shell setup
 cd ~/.dotfiles
 cp -f nix-env.fish/conf.d/nix-env.fish fish/.config/fish/conf.d
@@ -50,21 +49,19 @@ cp -f nix-env.fish/conf.d/nix-env.fish fish/.config/fish/conf.d
 # Stow dotfiles
 stow bash
 stow fish
-:q
-:qa
 stow git
 stow nvim
 stow tmux
 
 # Neovim setup
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-@ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-
-cd ~/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim || return
-make
-cd ~
-
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+# @ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+#
+# cd ~/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim || return
+# make
+# cd ~
+#
 fish
 
 # Misc
